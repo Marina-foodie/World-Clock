043173ba8aaf06af886cc695bb5d84b35f020ada
Marina-foodie <marina.ortner@gmail.com>
2025-06-15 16:52:45 +0200

    set the current time location

--- index.js
+++ index.js
@@ -18,7 +18,10 @@ setInterval(function () {
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current city") cityTimeZone = moment.tz.guess();
+
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+
   let cityTime = moment().tz(cityTimeZone);
   let TimeContainerElement = document.querySelector("#Time-Container");
   TimeContainerElement.innerHTML = `
